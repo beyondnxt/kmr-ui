aea73d2b6da8eaecb506105cac1aea4af641a8f0
Ajay-g-s <ajayraina57@gmail.com>
2024-05-14 23:53:34 +0530

    Role, Customer

--- src/app/master/pages/customer/customer.component.ts
+++ src/app/master/pages/customer/customer.component.ts
@@ -3,37 +3,49 @@ import * as data from './customer-data';
 import { AddCustomerComponent } from './components/add-customer/add-customer.component';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteModalComponent } from 'src/app/shared/components/delete-modal/delete-modal.component';
+import { CustomerService } from 'src/app/providers/customer/customer.service';
+import { CommonService } from 'src/app/providers/common/common.service';
+import { CustomerHelper } from './customer-helper';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.scss']
+  styleUrls: ['./customer.component.scss'],
+  providers: [CustomerHelper]
 })
 export class CustomerComponent {
-  constructor(private dialog: MatDialog,) { }
+  constructor(private dialog: MatDialog, private commonService: CommonService, private customerService: CustomerService, private customerHelper: CustomerHelper) { }
 
   tableHeaders = data.tableHeaders;
-  tableValues = data.tableValues;
+  tableValues: any = [];
   fixedTableHeader = data.fixedTableHeaders;
+  apiLoader = false;
+  totalCount = 0;
+
+  ngOnInit() {
+    this.getCustomer();
+  }
+
   addCompany() {
     this.openPopup();
   }
-  delete() {
+  delete(id: string) {
     this.dialog.open(DeleteModalComponent, {
       width: '650px',
       height: 'max-content',
+      data:id,
       disableClose: true,
       panelClass: 'delete-dialog-container',
     }).afterClosed().subscribe((res: any) => {
       if (res) {
-        console.log(res)
+        this.deleteCustomer(id)
       }
     });
   }
-  edit() {
-    this.openPopup(true);
+  edit(value: any) {
+    this.openPopup(value);
   }
-  openPopup(edit?: boolean) {
+  openPopup(edit?: any) {
     this.dialog.open(AddCustomerComponent, {
       width: '650px',
       height: 'max-content',
@@ -45,5 +57,38 @@ export class CustomerComponent {
         console.log(res)
       }
     });
+  }
+  pagination(pageData: any) {
+    this.getCustomer(pageData);
+  }
+
+  search(key: any) {
+    this.getCustomer('', `&value=${key}`)
+  }
+
+  getCustomer(query?: any, searchQuery?: string) {
+    this.apiLoader = true;
+    this.customerService.getCustomer(query, searchQuery).subscribe({
+      next: (res) => {
+        this.tableValues = this.customerHelper.mapCustomer(res.data);
+        this.totalCount = res.totalCount;
+        this.apiLoader = false;
+      },
+      error: (err) => {
+        this.apiLoader = false;
+        this.commonService.notification('Failed', 'Failed to get data', 'fail')
+      },
+    })
+  }
+
+  deleteCustomer(id: string) {
+    this.customerService.deleteCustomer(id).subscribe({
+      next: (res) => {
+        this.getCustomer();
+        this.commonService.notification('Success', 'Deleted Successfully', 'success')
+      }, error: (err) => {
+        this.commonService.notification('Failed', 'Failed to delete, please try again', 'fail')
+      },
+    })
   }
 }
---
+++ src/app/providers/customer/customer.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CommonService } from '../common/common.service';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(public http: HttpClient,public commonService:CommonService) { }
+  baseUrl = environment.KRM_BASE_URL;
+
+  createCustomer(payload: any): Observable<any> {
+    return this.http.post(this.baseUrl + '/customer', payload);
+  }
+
+  updateCustomer(payload: any, id:string): Observable<any> {
+    return this.http.put(this.baseUrl + `/customer/${id}`, payload);
+  }
+
+  deleteCustomer(id:string): Observable<any> {
+    return this.http.delete(this.baseUrl + `/customer/${id}`);
+  }
+
+  getCustomer(query:any,searchKey:string|any): Observable<any> {
+    return this.http.get(this.baseUrl + `/customer?page=${query?.pageNo || 1}&limit=${query?.pageLimit || this.commonService.calculatePaginationVal()}${searchKey ? searchKey : ''}`);
+  }
+
+  getAllMainCustomer(): Observable<any> {
+    return this.http.get(this.baseUrl + `/main-customer/all`);
+  }
+
+  getAllSalesLead(): Observable<any> {
+    return this.http.get(this.baseUrl + `/saleslead/all`);
+  }
+
+}
